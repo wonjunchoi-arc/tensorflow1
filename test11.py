cc9f9ad674f54876e1af70f319c87995cb7e1687
jun <qlszkd@naver.com>
2022-01-05 18:02:05 +0900

    update

--- test11.py
+++ test11.py
@@ -1,1 +1,21 @@
 print("hello")
+import torch
+# print(torch.cuda.is_available())
+# print(torch.cuda.device_count())
+# print(torch.cuda.get_device_name(torch.cuda.current_device()))
+USE_CUDA = torch.cuda.is_available()
+print(USE_CUDA)
+
+device = torch.device('cuda:0' if USE_CUDA else 'cpu')
+print('학습을 진행하는 기기:',device)
+
+print('cuda index:', torch.cuda.current_device())
+
+print('gpu 개수:', torch.cuda.device_count())
+
+print('graphic name:', torch.cuda.get_device_name())
+
+cuda = torch.device('cuda')
+
+print(cuda)
+
